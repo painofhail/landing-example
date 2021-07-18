c2ea051911bf72c6d730a327356076a153bd7800
painofhail <painofhail@yandex.ru>
2021-07-18 12:04:50 +0300

    log line fix

--- src/script/slider/splide.js
+++ src/script/slider/splide.js
@@ -6,7 +6,8 @@ new Splide( '.splide', {
 	heightRatio: 0,
 	start: 0,
 	arrows : true,
-	'arrowPath': 'M34.15 17.435L17.073 34.594l-1.642-1.65 14.27-14.34H0V16.27h29.705L15.432 1.925l1.641-1.65 17.076 17.16z',
+	'arrowPath':
+		'M34.15 17.435L17.073 34.594l-1.642-1.65 14.27-14.34H0V16.27h29.705L15.432 1.925l1.641-1.65 17.076 17.16z',
 	classes: {
 		arrows			: 'splide__arrows',
 		arrow 			: 'splide__arrow slider__arrow',
